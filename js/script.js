5450b292dbd9852280bf224f7b1a8db56ac95311
JesusChiro <jesus_chiroque@hotmail.com>
2023-04-12 17:59:04 -0500

    web telecomv2 v1

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 
         if(status == "none"){
             $("#nav").css("display","block");
-        }else{
+                 }else{
             $("#nav").css("display","none");
         }
 
