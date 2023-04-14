b33394f5129c2f40b67e00980335e88af01c911c
JesusChiro <jesus_chiroque@hotmail.com>
2023-04-14 12:15:31 -0500

    nmtelecom v2

--- js/script.js
+++ js/script.js
@@ -1,16 +1,60 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  $("#cerrar").click(function () {
+    var status = $("#nav").css("display");
 
-    $("#cerrar").click(function(){
-        
-        var status = $("#nav").css("display");
-
-        if(status == "none"){
-            $("#nav").css("display","block");
-                 }else{
-            $("#nav").css("display","none");
-        }
-
-    })
+    if (status == "none") {
+      $("#nav").css("display", "block");
+    } else {
+      $("#nav").css("display", "none");
+    }
+  });
 
 });
-
+$(document).ready(function () {
+    $("#linknav").click(function () {
+      var status = $("#nav").css("display");
+  
+      if (status == "block") {
+        $("#nav").css("display", "none");
+      } else {
+        $("#nav").css("display", "block");
+      }
+    });
+  
+  });
+  $(document).ready(function () {
+    $("#linknav2").click(function () {
+      var status = $("#nav").css("display");
+  
+      if (status == "block") {
+        $("#nav").css("display", "none");
+      } else {
+        $("#nav").css("display", "block");
+      }
+    });
+  
+  });
+  $(document).ready(function () {
+    $("#linknav3").click(function () {
+      var status = $("#nav").css("display");
+  
+      if (status == "block") {
+        $("#nav").css("display", "none");
+      } else {
+        $("#nav").css("display", "block");
+      }
+    });
+  
+  });
+  $(document).ready(function () {
+    $("#linknav4").click(function () {
+      var status = $("#nav").css("display");
+  
+      if (status == "block") {
+        $("#nav").css("display", "none");
+      } else {
+        $("#nav").css("display", "block");
+      }
+    });
+  
+  });
