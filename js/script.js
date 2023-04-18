efcdc079b53400b7f46caaf53964480558be0c48
JesusChiro <jesus_chiroque@hotmail.com>
2023-04-17 19:30:15 -0500

    nmtelecom v5

--- js/script.js
+++ js/script.js
@@ -8,53 +8,60 @@ $(document).ready(function () {
       $("#nav").css("display", "none");
     }
   });
-
 });
-$(document).ready(function () {
-    $("#linknav").click(function () {
-      var status = $("#nav").css("display");
-  
-      if (status == "block") {
-        $("#nav").css("display", "none");
-      } else {
-        $("#nav").css("display", "block");
-      }
-    });
-  
-  });
-  $(document).ready(function () {
-    $("#linknav2").click(function () {
-      var status = $("#nav").css("display");
-  
-      if (status == "block") {
-        $("#nav").css("display", "none");
-      } else {
-        $("#nav").css("display", "block");
-      }
-    });
-  
-  });
-  $(document).ready(function () {
-    $("#linknav3").click(function () {
-      var status = $("#nav").css("display");
-  
-      if (status == "block") {
-        $("#nav").css("display", "none");
-      } else {
-        $("#nav").css("display", "block");
-      }
-    });
-  
-  });
-  $(document).ready(function () {
-    $("#linknav4").click(function () {
-      var status = $("#nav").css("display");
-  
-      if (status == "block") {
-        $("#nav").css("display", "none");
-      } else {
-        $("#nav").css("display", "block");
-      }
-    });
-  
-  });
+// $(document).ready(function () {
+//     $("#linknav").click(function () {
+//       var status = $("#nav").css("display");
+
+//       if (status == "block") {
+//         $("#nav").css("display", "none");
+//       } else {
+//         $("#nav").css("display", "block");
+//       }
+//     });
+
+//   });
+//   $(document).ready(function () {
+//     $("#linknav2").click(function () {
+//       var status = $("#nav").css("display");
+
+//       if (status == "block") {
+//         $("#nav").css("display", "none");
+//       } else {
+//         $("#nav").css("display", "block");
+//       }
+//     });
+
+//   });
+//   $(document).ready(function () {
+//     $("#linknav3").click(function () {
+//       var status = $("#nav").css("display");
+
+//       if (status == "block") {
+//         $("#nav").css("display", "none");
+//       } else {
+//         $("#nav").css("display", "block");
+//       }
+//     });
+
+//   });
+//   $(document).ready(function () {
+//     $("#linknav4").click(function () {
+//       var status = $("#nav").css("display");
+
+//       if (status == "block") {
+//         $("#nav").css("display", "none");
+//       } else {
+//         $("#nav").css("display", "block");
+//       }
+//     });
+
+//   });
+let previousTitle = document.title;
+window.addEventListener("blur", () => {
+  previousTitle = document.title;
+  document.title = "¡No te Vayas! ¡Vuelve! 😐 ";
+});
+window.addEventListener("focus", () => {
+  document.title = previousTitle;
+});
